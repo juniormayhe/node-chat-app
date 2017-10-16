1d861317ed48d7b29011bc5009d5430af1a52e5c
Wanderley Mayhe Junior <juniormayhe@gmail.com>
2017-10-15 19:33:18 -0500

    connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,47 @@
 const path = require('path');
+const http= require('http');
 const express= require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-var app= express();
+var app     = express();
 app.use(express.static(publicPath));
-app.listen(process.env.PORT, ()=>{
+var server  = app.listen(port, ()=>{
     console.log(`server is app at ${port}`);
 });
+
+var io = require('socket.io').listen(server);
+io.on('connection', (socket)=> {
+    console.log('-> new user connected');
+    socket.on('disconnect', ()=>{
+        console.log('-> user was disconnected');
+    });
+});
+console.log('Express + Socket IO server ----------------');
+
+
+// const path = require('path');
+// const http = require('http');
+// const express = require('express');
+// const socketIO = require('socket.io');
+
+// const publicPath = path.join(__dirname, '../public');
+// const port = process.env.PORT || 3000;
+// var app = express();
+// var server = http.createServer(app);
+// var io = socketIO(server);
+
+// app.use(express.static(publicPath));
+
+// io.on('connection', (socket) => {
+//   console.log('New user connected');
+
+//   socket.on('disconnect', () => {
+//     console.log('User was disconnected');
+//   });
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server is up on ${port}`);
+// });
